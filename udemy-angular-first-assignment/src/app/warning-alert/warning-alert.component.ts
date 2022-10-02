bb226425b659068edde0eddb2350825075b0e10a
David Foster <davidfoster1990@hotmail.co.uk>
2022-10-02 21:35:33 +0100

    first assignment

--- udemy-angular-first-assignment/src/app/warning-alert/warning-alert.component.ts
+++ udemy-angular-first-assignment/src/app/warning-alert/warning-alert.component.ts
@@ -1,1 +1,14 @@
-export class {}
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-warning-alert',
+  template: `<h3>Warning</h3>`,
+  styles: [
+    `
+      h3 {
+        color: red;
+      }
+    `,
+  ],
+})
+export class WarningAlertComponent {}
