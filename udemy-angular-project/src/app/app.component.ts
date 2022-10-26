55d90462f290654228fa7caeeffe70a82a63c8cb
David Foster <davidfoster1990@hotmail.co.uk>
2022-10-26 19:48:34 +0100

    project tidy-up

--- udemy-angular-project/src/app/app.component.ts
+++ udemy-angular-project/src/app/app.component.ts
@@ -1,16 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styles: [
-    `
-      h3 {
-        color: red;
-      }
-    `,
-  ],
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  name = 'David';
-}
+export class AppComponent {}
