1e14cd42b5a5fe0844abc6f86db17701396d693b
David Foster <davidfoster1990@hotmail.co.uk>
2023-02-16 17:49:00 +0000

    course progress

--- udemy-angular-project/src/app/app.module.ts
+++ udemy-angular-project/src/app/app.module.ts
@@ -8,8 +8,8 @@ import { RecipeBook } from './recipe-book/recipe-book.component';
 import { RecipeList } from './recipe-book/recipe-list/recipe-list.component';
 import { RecipeItem } from './recipe-book/recipe-list/recipe-item/recipe-item.component';
 import { RecipeDetail } from './recipe-book/recipe-detail/recipe-detail.component';
-import { ShoppingList } from './shopping-list/shopping-list/shopping-list.component';
-import { ShoppingListEdit } from './shopping-list/shopping-list/shopping-list-edit/shopping-list-edit.component';
+import { ShoppingList } from './shopping-list/shopping-list.component';
+import { ShoppingListEdit } from './shopping-list/shopping-list-edit/shopping-list-edit.component';
 
 @NgModule({
     declarations: [AppComponent, HeaderComponent, RecipeBook, RecipeList, RecipeItem, RecipeDetail, ShoppingList, ShoppingListEdit],
--- udemy-angular-project/src/app/recipe-book/recipe-list/recipe-list.component.ts
+++ udemy-angular-project/src/app/recipe-book/recipe-list/recipe-list.component.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
     selector: 'app-recipe-list',
     templateUrl: './recipe-list.component.html',
     styleUrls: ['./recipe-list.component.css'],
 })
-export class RecipeList {}
+export class RecipeList {
+    recipes: Recipe[] = [
+        new Recipe('A test recipe', 'this is simply a test', 'https://ichef.bbci.co.uk/food/ic/food_16x9_832/recipes/sausage_pasta_91648_16x9.jpg'),
+        new Recipe('A test recipe', 'this is simply a test', 'https://ichef.bbci.co.uk/food/ic/food_16x9_832/recipes/sausage_pasta_91648_16x9.jpg'),
+    ];
+}
